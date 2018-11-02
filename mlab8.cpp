c48ce1e63f7bbfdcd6fef63fa59bded066c24155
hrndz24 <40834596+hrndz24@users.noreply.github.com>
2018-11-02 21:53:56 +0300

    Add files via upload

--- mlab8.cpp
+++ mlab8.cpp
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <conio.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <conio.h> 
+#include <locale.h>
 
-// checks if salary is lower than the one before
+// checks if salary is lower than the one before 
 bool checkSalary(char salary[8], char salary_before[8]) {
 	if (strlen(salary) < strlen(salary_before)) {
 		return true;
@@ -26,9 +27,9 @@ bool checkSalary(char salary[8], char salary_before[8]) {
 	}
 }
 
-// checks if GPA is greater tha the one before
+// checks if GPA is greater tha the one before 
 bool checkGPA(char gpa[5], char gpa_before[5]) {
-	
+
 	if (strlen(gpa) > strlen(gpa_before)) {
 		return true;
 	}
@@ -50,14 +51,18 @@ bool checkGPA(char gpa[5], char gpa_before[5]) {
 	}
 }
 
-// checks if salary is less than 2 min salaries
-bool isLessThanMin(char *salary) {
+// checks if salary is less than 2 min salaries 
+bool isLessThanMin(char salary[6]) {
+
 	if (strlen(salary) < 3) {
 		return true;
 	}
 	else if (strlen(salary) == 3) {
-		if ((int)salary[0] < 6) {
+		if ((int)salary[0] < 54) {
 			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	else {
@@ -65,8 +70,8 @@ bool isLessThanMin(char *salary) {
 	}
 }
 
-// cuts out a piece from line
-char* pieceOfLine(char *line, int start, int end) {	
+// cuts out a piece from line 
+char* pieceOfLine(char *line, int start, int end) {
 	char *piece = (char *)malloc(sizeof(char) * 20);
 	int p, l;
 	for (p = 0, l = start; l < end; l++, p++) {
@@ -77,15 +82,15 @@ char* pieceOfLine(char *line, int start, int end) {
 }
 
 int findStartSalary(char *student) {
-	int startSalary = 0;	
+	int startSalary = 0;
 	for (int i = 0; i < strlen(student); i++) {
 
 		if ((int)student[i] == 32) {
-			// sets startSalary every time space is met
-			// in the end its value will be after the last space
+			// sets startSalary every time space is met 
+			// in the end its value will be after the last space 
 			startSalary = i + 1;
 		}
-	}	
+	}
 	return startSalary;
 }
 
@@ -94,8 +99,8 @@ int findEndGPA(char *student) {
 	for (int i = 0; i < strlen(student); i++) {
 
 		if ((int)student[i] == 32) {
-			// sets endGPA every time space is met
-			// in the end its value will be the last space
+			// sets endGPA every time space is met 
+			// in the end its value will be the last space 
 			endGPA = i;
 		}
 	}
@@ -107,14 +112,14 @@ int findStartGPA(char *student) {
 	for (int i = 0; i < strlen(student); i++) {
 		// when a dot(from GPA) is met 
 		if ((int)student[i] == 46) {
-			// if 3 chars away is space
+			// if 3 chars away is space 
 			if ((int)student[i - 3] == 32) {
-				// '.' is 2 chars away from the first character of GPA
+				// '.' is 2 chars away from the first character of GPA 
 				startGPA = i - 2;
 				break;
 			}
 			else {
-				// '.' is 1 char away from the first character of GPA
+				// '.' is 1 char away from the first character of GPA 
 				startGPA = i - 1;
 				break;
 			}
@@ -123,11 +128,11 @@ int findStartGPA(char *student) {
 	return startGPA;
 }
 
-// finds where normal salaries start
+// finds where normal salaries start 
 int findDivider(char **students, int length) {
-	int divider;
+	int divider=0;
 	for (int i = 0; i < length; i++) {
-		int endSalary1 = strlen(students[i]) - 2;
+		int endSalary1 = strlen(students[i]);
 		char *salary = pieceOfLine(students[i], findStartSalary(students[i]), endSalary1);
 		if (!(isLessThanMin(salary))) {
 			divider = i;
@@ -137,22 +142,23 @@ int findDivider(char **students, int length) {
 	return divider;
 }
 
-// prints array sorted by GPA
+// prints array sorted by GPA 
 void sortByGPA(char **students, int startIndex, int endIndex) {
-	for (int i = startIndex; i < endIndex-1; i++) {
-		for (int j = startIndex+1; j < endIndex; j++) {			
+	for (int i = startIndex; i < endIndex - 1; i++) {
+		for (int j = startIndex + 1; j < endIndex; j++) {
 			char *GPA = pieceOfLine(students[j], findStartGPA(students[j]), findEndGPA(students[j]));
-			char *GPA_before = pieceOfLine(students[j-1], findStartGPA(students[j-1]), findEndGPA(students[j-1]));
+			char *GPA_before = pieceOfLine(students[j - 1], findStartGPA(students[j - 1]), findEndGPA(students[j - 1]));
 			if (checkGPA(GPA, GPA_before)) {
-				// changes these two students
+				// changes these two students 
 				char *zam = students[j];
 				students[j] = students[j - 1];
 				students[j - 1] = zam;
 			}
 		}
 	}
+	
 	for (int i = startIndex; i < endIndex; i++) {
-		printf("%s\n", students[i]);
+		printf("\n%s", students[i]);
 	}
 }
 
@@ -163,48 +169,101 @@ char** sortBySalary(char **students, int length) {
 			int endSalary_before = strlen(students[j - 1]);
 			char *salary = pieceOfLine(students[j], findStartSalary(students[j]), endSalary);
 			char *salary_before = pieceOfLine(students[j - 1], findStartSalary(students[j - 1]), endSalary_before);
-			// if salary is less than the one before
-			if (checkSalary(salary, salary_before)) {
-				// changes these two students
-				char *zam = students[j];
-				students[j] = students[j - 1];
-				students[j - 1] = zam;
-			}
+			// if salary is less
+			//than the one before
+				if (checkSalary(salary, salary_before)) {
+					// changes these two students 
+					char *zam = students[j];
+					students[j] = students[j - 1];
+					students[j - 1] = zam;
+				}
 		}
 	}
+	
 	return students;
 }
 
-int main() {
-
-	FILE *f2;
-	if (!(f2 = fopen("e:\\work\\students.txt", "r + b")))
-	{
-		puts("\n Файл не создан!");
-		return 0;
-	}
-	// just accept it
-	char **students;
-	students = new char*[24];
-	//creating every student array
-	for (int i = 0; i < 24; i++) {
-		students[i] = new char[256];
-	}
-	//reads the file and adds every line to students array
-	int k = 0;
-	char line[256];
-	while (fgets(students[k], 256, f2) != NULL)	{
-		students[k][256] = '\0';
-		k += 1;
-	}
-	// length of the array
-	int length = k;	
-	// sorts the array by salary
-	students = sortBySalary(students, length);
-	// where people with normal salaries start
-	int divider = findDivider(students, length);
-	// sorts the array before the rich by GPA
-	sortByGPA(students, 0, divider);
-	// sorts the array with the rich by GPA
-	sortByGPA(students, divider, length);
+int enterCode() {
+	int code;
+	puts(" Создание - 1");
+	puts(" Просмотр - 2");
+	puts(" Добавление - 3");
+	puts(" Выход - 0");
+	scanf("%d", &code);
+	return code;
 }
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	while (1) {
+		int code = enterCode();
+		//смещает курсор (считывания) в конец, чтобы не запоминал 
+		// введённые до этого значения
+		fseek(stdin, 0, SEEK_END);
+		switch (code) {
+		case 0:
+			return 0;
+		case 1:
+			// создаёт новый файл(ни за что не меняй это имя на существующий
+			// файл, иначе он сотрёт там всю инфу)
+			FILE *f1;
+			f1 = fopen("e:\\work\\new_students.txt", "w + b");
+			printf("\nA new file was created in e:\\\\work");
+			fclose(f1);
+			break;
+		case 3:
+			FILE *f3;
+			f3 = fopen("e:\\work\\students.txt", "a + b");
+			char s[256];
+			printf("Enter a student in a format 'Name Group GPA salary': ");
+			fgets(s, 256, stdin);
+			// ставит конец строки, я не знаю, почему -1
+			// методом проб и ошибок конец должен быть там
+			s[strlen(s)-1] = '\0';
+			// \r r значит, что он обрабатывает дальше символы 
+			// иначе он бы в файл записал \n...
+			fprintf(f3, "\r\n%s", s);
+			fclose(f3);
+			break;
+		case 2: FILE *f2;
+			if (!(f2 = fopen("e:\\work\\students.txt", "r + b")))
+			{
+				puts("\n Error opening file!");
+				return 0;
+			}
+			// just accept it 
+			char **students;
+			students = new char*[24];
+			//creating every student array 
+			for (int i = 0; i < 24; i++) {
+				students[i] = new char[256];
+			}
+			//reads the file and adds every line to students array 
+			int k = 0;
+		
+			while (fgets(students[k], 256, f2) != NULL) {
+				for (int i = 0; i < 256; i++) {
+					// when '\n' is met sets the end of line
+					if ((int)students[k][i] == 13) {
+						students[k][i] = '\0';						
+					}
+				}
+				k += 1;
+			}
+
+			// length of the array 
+			int length = k;
+			
+			// sorts the array by salary 
+			students = sortBySalary(students, length);
+			// where people with normal salaries start 
+			int divider = findDivider(students, length);
+			
+			// sorts the array before the rich by GPA 
+			sortByGPA(students, 0, divider);
+			// sorts the array with the rich by GPA 
+			sortByGPA(students, divider, length);
+			fclose(f2);
+		} // Конец Switch 2
+	} // Конец While(1) 
+} // Конец программы
